a0a17208e7c435380c95d79b53d35200860f3c56
Shahriar Elias <shahriar998@gmail.com>
2019-10-24 17:52:35 -0400

    got osy's page working

--- client/src/components/containers/AdminLogin.js
+++ client/src/components/containers/AdminLogin.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import Header from "../Header";
+import Wrapper from "../Wrapper";
+import Footer from "../Footer";
 
 class AdminLogin extends Component {
 
     render() {
         return (
-            <> 
-            <h1>This is my admin Page</h1>
-            </>
+            <div className="wrapper" id="wrapper">
+       
+           
+            <Header />
+            <Wrapper />
+
+
+            <Footer />
+       </div>
         );
     }
 }
