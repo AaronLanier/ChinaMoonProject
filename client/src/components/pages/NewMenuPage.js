055f70aef72e1dfaadef1ec70a3b3e22f129ac84
Shahriar Elias <shahriar998@gmail.com>
2019-10-29 20:43:11 -0400

    latest changes to push it to heroku

--- client/src/components/pages/NewMenuPage.js
+++ client/src/components/pages/NewMenuPage.js
@@ -37,67 +37,89 @@ class NewMenuPage extends Component {
       });
   };
 
-  handleMenuTypeChange =(e)=> {
-      console.log("what we choose ",e.target.value);
-      this.setState({menuType:e.target.value})
-  }
-  handleCategoryNameChange =(e)=>{
-
-    this.setState({categoryName:e.target.value})
-  }
+  handleMenuTypeChange = e => {
+    console.log("what we choose ", e.target.value);
+    this.setState({ menuType: e.target.value });
+  };
+  handleCategoryNameChange = e => {
+    this.setState({ categoryName: e.target.value });
+  };
 
   render() {
     return (
-      <div>
-        <h1>This is my new menu</h1>
-        <select name = "menuType" value={this.state.menuType} onChange={this.handleMenuTypeChange}>
-          <option value="All Day Menu">All Day Menu</option>
-          <option value="Lunch Specials">Lunch Specials</option>
-          <option value="Family Dinners">Family Dinners</option>
-        </select>
-        <select name = "categoryName" value={this.state.categoryName} onChange={this.handleCategoryNameChange}>
-          <option value="Appetizers">Appetizers</option>
-          <option value="Chicken">Chicken</option>
-          <option value="Beef">Beef</option>
-          <option value="Sea Food">Sea Food</option>
-          <option value="Pork">Pork</option>
-          <option value="Vegetables">Vegetables</option>
-          <option value="Lo Mein">Lo Mein</option>
-          <option value="House Special">House Special</option>
-          <option value="Chow Mein">Chow Mein</option>
-          <option value="Egg FU Yung and Fried Rice">Egg FU Yung and Fried Rice</option>
-          <option value="Combination Dinner">Combination Dinner</option>
-          <option value="Kid's Menu">Kid's Menu</option>
-        </select>
-        <form>
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Item Name"
-              onChange={this.handleChange}
-              name="itemName"
-              value={this.state.itemName}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="number"
-              placeholder="price"
-              onChange={this.handleChange}
-              name="price"
-              value={this.state.price}
-            />
-          </div>
-          <div className="form-group">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={this.handleFormSubmit}
+      <div className="jumbotron">
+        <div className="row">
+          <div className="col">
+            <h1>ADD A MENU HERE</h1>
+            <select
+              name="menuType"
+              value={this.state.menuType}
+              onChange={this.handleMenuTypeChange}
             >
-              Submit
-            </button>
+              <option value="All Day Menu">All Day Menu</option>
+              <option value="Lunch Specials">Lunch Specials</option>
+              <option value="Family Dinners">Family Dinners</option>
+            </select>
           </div>
-        </form>
+        </div>
+        
+        <div className="row">
+          <div className="col">
+            <select
+              name="categoryName"
+              value={this.state.categoryName}
+              onChange={this.handleCategoryNameChange}
+            >
+              <option value="Appetizers">Appetizers</option>
+              <option value="Chicken">Chicken</option>
+              <option value="Beef">Beef</option>
+              <option value="Sea Food">Sea Food</option>
+              <option value="Pork">Pork</option>
+              <option value="Vegetables">Vegetables</option>
+              <option value="Lo Mein">Lo Mein</option>
+              <option value="House Special">House Special</option>
+              <option value="Chow Mein">Chow Mein</option>
+              <option value="Egg FU Yung and Fried Rice">
+                Egg FU Yung and Fried Rice
+              </option>
+              <option value="Combination Dinner">Combination Dinner</option>
+              <option value="Kid's Menu">Kid's Menu</option>
+            </select>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Item Name"
+                  onChange={this.handleChange}
+                  name="itemName"
+                  value={this.state.itemName}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="number"
+                  placeholder="price"
+                  onChange={this.handleChange}
+                  name="price"
+                  value={this.state.price}
+                />
+              </div>
+              <div className="form-group">
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={this.handleFormSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
